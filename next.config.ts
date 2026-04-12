173e5b9bfdfaabcac856bda360729b0e3c1b5365
nithin.nm <nithin.nm@pitsolutions.com>
2026-04-13 00:14:35 +0530

    fix: resolve github pages basepath routing

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/DigiGarage",
   images: {
     unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
       },
     ],
   },
